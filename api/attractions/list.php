0175ec51aced77049e13a9fadbccd3b08d094106
Louis TARDY <louistardy58@gmail.com>
2019-04-15 10:04:21 +0200

    --useless files

--- api/attractions/list.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('Content-Type: application/json');
-
-require_once __DIR__ . '/../../services/AttractionService.php';
-
-echo json_encode(AttractionService::getInstance()->allAttractions());
-
-?>
--- api/pass/list.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('Content-Type: application/json');
-
-require_once __DIR__ . '/../../services/PASSService.php';
-
-echo json_encode(PASSService::getInstance()->allPASS());
-
-?>
